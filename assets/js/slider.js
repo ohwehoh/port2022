c5dcabe1704e2c6f3ca98c3efdadd7a3c77da3a5
ohwehoh <ohwehoh@naver.com>
2022-05-25 12:34:11 +0900

    22.05.25.

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -5,26 +5,26 @@ let sliderCount = null;
 let sliderClone = null;
 let currentIndex = 0;
 
-function varInit(overV){
-    console.log("varInit: " + overV);
-    sliderInner = document.querySelector(".slider__inner");
-    slider = document.querySelectorAll(".slider");
+function varInit(overV, slider__inner){
+    sliderInner = slider__inner;
+    slider = sliderInner.querySelectorAll(".slider");
     sliderCount = slider.length;
     sliderClone = sliderInner.firstElementChild.cloneNode(true);
-    overV ? 0 : sliderInner.appendChild(sliderClone);
-    overV = true;
+    if(!overV){
+        sliderInner.appendChild(sliderClone);
+        overV = true;
+    }
     return overV;
 }
 
 function sliderEffectInit() {
-    console.log("init")
     // clearInterval(timer);
     const sliderWindow = document.querySelector(".showBox");
 
     let sliderWidth = sliderWindow.offsetWidth;
 
     const info = {
-        sliderCount: sliderCount,
+        // sliderCount: sliderCount,
         sliderWidth: sliderWidth,
         sliderInner: sliderInner,
     }
@@ -37,14 +37,12 @@ function sliderEffectInit() {
 }
 
 function sliderEffectMove(info) {
-    console.log("move, width: " + info.sliderWidth);
     currentIndex++;
-    console.log("index: " + currentIndex);
     info.sliderInner.style.transition = "all 0.3s";
     info.sliderInner.style.transform = `translateX(-${info.sliderWidth*currentIndex}px)`;
 
     //마지막 사진에 갔을때
-    if (currentIndex == info.sliderCount) {
+    if (currentIndex == 7) {
         setTimeout(() => {
             info.sliderInner.style.transition = "0s";
             info.sliderInner.style.transform = 'translateX(0)';
